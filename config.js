7f9b25c01240a0f18010a48c73532a9ab53b58ba
Dy1312 <hoangduy130106@gmail.com>
2024-10-23 19:15:54 +0700

    test config.js

--- config.js
+++ config.js
@@ -1,19 +1,13 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    introTitle: 'Gửi bé nhí 9 tủi nè.',
+    title: 'bé nhí đừng giận tui nữa nhaa, tui biết lỗi ròiiii màaaa',
+    btnYes: 'oke tha lỗi cho đó, hok giận nữa',
+    btnNo: 'hok!!!! -.-',
+    question:'mai cho tui chở bé nhí đi học nhaaa ',
+    btnReply: 'tất nhiên là cho ròi',
+    reply: 'okeeeee',
+    mess: 'tui biết mà 🥰. Yêu bé nhiều nhiều 😘😘',
+    messDesc: 'sáng mai tui qua đón nhé .',
+    btnAccept: 'Okiiiii lun',
+    messLink: 'https://www.facebook.com/profile.php?id=100044188250260
 }
